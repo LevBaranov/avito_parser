f5c1db59388cc65fb622502e38121fd242be6fd1
LevBaranov <lev.l.baranov@gmail.com>
2021-02-14 14:28:20 +0500

    added method get_region_id and get_category_ids

--- parser.py
+++ parser.py
@@ -1,57 +1,51 @@
-import random
-import requests
-
-from bs4 import BeautifulSoup
+import json
+import httpx
+#from urllib.request import quote
 
 class Parser():
     '''Наша рабочая лошадка'''
 
-    def __init__(self, url):
-        self.url = url
-
-    def get_proxy():
-        proxy = requests.get(
-            'https://gimmeproxy.com/api/getProxy?country=RU&get=true&supportsHttps=true&protocol=http')
-        proxy_json = json.loads(proxy.content)
-        if proxy.status_code != 200 and 'ip' not in proxy_json:
-            raise RequestException
-        else:
-            return proxy_json['ip'] + ':' + proxy_json['port']
-
-    def get_html(self, proxy=None):
-        USER_AGENTS = [
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-            'Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv)',
-            'Mozilla/5.0 (X11; Linux i686; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        ]
-        headers = {
-            'User-Agent': random.choice(USER_AGENTS)
+    def __init__(self, key):
+        self.key = key
+        self.avito_urls = {
+            'region_id': f'https://m.avito.ru/api/1/slocations',
+            'categories_info': f'https://m.avito.ru/api/2/search/main?&locationId=',
+            'avito_main': 'https://avito.ru'
         }
-        proxy_list = [get_proxy()]
-        proxies = {
-            'http': 'http://91.195.130.237:8080',
-            'https': 'https://91.195.130.237:8080'
-        }
-        response = requests.get(self.url, headers=headers, proxies=proxies, timeout=20)
 
-        self.html = BeautifulSoup(response.content, 'lxml')
-        return self.html 
-    
+    def get_json_by_request(self, url, params):
+        try:
+            resp = httpx.get(url, params=params)
+            json_content = resp.json()
+            if 'status' in json_content.keys():
+                if json_content['status'] == 'internal-error':
+                    print('internal-error')
+                    self.get_json_by_request(url, params)
+            return json_content
+        except httpx.exceptions.ProxyError:
+            sleep(0.001)
+            self.get_json_by_request(url,params)
+
+    def get_region_id(self, region):
+        params = {
+            'key':f'{self.key}',
+            'q':f'{region}',
+            'limit':10
+        }
+        url = f'{self.avito_urls["region_id"]}'
+        json_content = self.get_json_by_request(url, params)
+        print(json_content)
+        locations = json_content['result']['locations']
+
+        return locations[0]['id']
+
     def __repr__(self):
         return "Parser('%s')" % (self.url)
 
 if __name__ == '__main__':
-    url = 'https://www.avito.ru/kazan/igry_pristavki_i_programmy/igrovye_pristavki-ASgBAgICAUSSAsoJ?q=ps4+pro'
-    parser = Parser(url)
-    response = parser.get_html() #, headers=headers, proxies=proxies, timeout=Config.REQUEST_TIMEOUT)
+    #url = 'https://m.avito.ru/api/1/slocations?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&locationId=621540&limit=10&q='
+    parser = Parser('af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir')
+    print(parser.get_region_id('Пермь'))
+    #response = parser.get_json_by_request() #, headers=headers, proxies=proxies, timeout=Config.REQUEST_TIMEOUT)
 
-    print(response)
+    #print(response)
